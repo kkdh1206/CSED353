4026cc524568bdc6165e429c3b83838baf21f1b9
Your Name <kkdh1206@naver.com>
2026-04-27 10:45:30 +0000

    save

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -12,29 +12,46 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {}; }
+size_t TCPConnection::remaining_outbound_capacity() const { return {_sender._stream.remaining_capacity()}; } // 남은용량 반환
 
-size_t TCPConnection::bytes_in_flight() const { return {}; }
+size_t TCPConnection::bytes_in_flight() const { return {_sender.bytes_in_flight()}; } // 보내는 중인데이터
 
-size_t TCPConnection::unassembled_bytes() const { return {}; }
+size_t TCPConnection::unassembled_bytes() const { return {_receiver.unassembled_bytes()}; } // 
 
-size_t TCPConnection::time_since_last_segment_received() const { return {}; }
+size_t TCPConnection::time_since_last_segment_received() const { return {_last_received_time}; } // 마지막으 받고 초기화되고 ticker가 돌아가고있음
 
-void TCPConnection::segment_received(const TCPSegment &seg) { DUMMY_CODE(seg); }
+void TCPConnection::segment_received(const TCPSegment &seg) { // 수신시
+    _receiver.segment_received(seg); 
+    _last_received_time = 0; // 받았으니까 이거 초기화
+    if (seg.header().syn) {
 
-bool TCPConnection::active() const { return {}; }
+    }
+}
 
-size_t TCPConnection::write(const string &data) {
+bool TCPConnection::active() const { return {_active}; }
+
+size_t TCPConnection::write(const string &data) { // 발송시
     DUMMY_CODE(data);
     return {};
 }
 
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
-void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); 
+    _last_received_time += ms_since_last_tick; // 시간흐르도록
+    _sender.tick(ms_since_last_tick); // 여기도 시간흐르도록
+    if (consecutive_retransmissions < TCPConfig::MAX_RETX_ATTEMPTS){
+        
 
-void TCPConnection::end_input_stream() {}
+    }
+}
 
-void TCPConnection::connect() {}
+void TCPConnection::end_input_stream() {
+    _sender.stream.end_input(); // 종료를 위해 이걸 호출
+}
+
+void TCPConnection::connect() { // 연결하는 함수 3hand shake
+
+}
 
 TCPConnection::~TCPConnection() {
     try {
--- writeups/assn4.md
+++ writeups/assn4.md
@@ -1,11 +1,11 @@
 Assignment 4 Writeup
 =============
 
-My name: [your name here]
+My name: 김도형
 
-My POVIS ID: [your povis id here]
+My POVIS ID: kkdh1206
 
-My student ID (numeric): [your student id here]
+My student ID (numeric): 20210740
 
 This assignment took me about [n] hours to do (including the time on studying, designing, and writing the code).
 
