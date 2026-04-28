2d3fce2239fdc834338e521971cd0ab5b5dc5db7
Your Name <kkdh1206@naver.com>
2026-04-28 15:28:51 +0000

    save

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -12,7 +12,7 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {_sender._stream.remaining_capacity()}; } // 남은용량 반환
+size_t TCPConnection::remaining_outbound_capacity() const { return {_sender.stream_in().remaining_capacity()}; } // 남은용량 반환
 
 size_t TCPConnection::bytes_in_flight() const { return {_sender.bytes_in_flight()}; } // 보내는 중인데이터
 
@@ -21,23 +21,45 @@ size_t TCPConnection::unassembled_bytes() const { return {_receiver.unassembled_
 size_t TCPConnection::time_since_last_segment_received() const { return {_last_received_time}; } // 마지막으 받고 초기화되고 ticker가 돌아가고있음
 
 void TCPConnection::segment_received(const TCPSegment &seg) { // 수신시
-    _receiver.segment_received(seg); 
-    _last_received_time = 0; // 받았으니까 이거 초기화
-    if (seg.header().syn) {
 
+    // 그냥 데이터면 last_received_time초기화
+    _last_received_time = 0; // 받았으니까 이거 초기화
+
+    // rst 면 걍 거부
+    if (seg.header().rst){ 
+         _active = false;
+        _sender.stream_in().set_error();
+        _receiver.stream_out().set_error();
+        return;
+    }
+    
+    // reciever 한테도 보내주기
+    _receiver.segment_received(seg); 
+    // ack(크기 > 0)면 잘받았다 ack보내주기
+    if (seg.header().ack){
+        auto window = seg.header().win;
+        auto ackno = seg.header().ackno;
+        _sender.ack_received(ackno,window); // 받았으니까 처리
+        // if(seg.length_in_sequence_space() ==0){
+        //     return; // 이건 데이터도없어서 다시 ack반환안해도됨
+        // }
+    } // synack일수도있으니까 ack먼저 처리
+
+    if (seg.header().syn && _sender.next_seqno_absolute()== 0) { // 처음 SYN에 대해서만 connect날리고 SYNACK는 패스시킴
+        connect(); // 연결요청
     }
 
+    // ack 보내주기 - 데이터가 있는거만
+    if (seg.length_in_sequence_space()>0){
+        _sender.fill_window(); // 데이터 보낼거 충전해둠
+        if(_sender.stream_in().buffer_empty()){ // 그래도 보낼게 없다 즉 거절
+            _sender.send_empty_segment(); // ackno전달용 pure ACK
+        }
+    }  
+    send_segment();
 
-
-
-
-
-
-
-    
 }
 
-bool TCPConnection::active() const { return {_active}; }
 
 size_t TCPConnection::write(const string &data) { // 발송시
     DUMMY_CODE(data);
@@ -50,12 +72,33 @@ size_t TCPConnection::write(const string &data) { // 발송시
     return {written_size};
 }
 
+bool TCPConnection::active() const {
+    if(!_active){
+        return false;
+    }
+    return true;
+}
+
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
 void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); 
+    if (!_active) {
+        return;  // 꺼져있으면 작동안함
+    }
+    
     _last_received_time += ms_since_last_tick; // 시간흐르도록
+
+    // if (10* <= _last_received_time){
+    //     _active = false;
+    // }
+
+
     _sender.tick(ms_since_last_tick); // 여기도 시간흐르도록
     if (_sender.consecutive_retransmissions() <= TCPConfig::MAX_RETX_ATTEMPTS){
+        _sender.fill_window(); // zero probing 보낼수도
         send_segment(); // 재전송 혹은 실패 seg를 전송
+
+        // 여기서 시간확인
+
     }
     else{
         _active = false;
@@ -68,6 +111,7 @@ void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_
         seg = queue.back(); // 젤뒤에 추가됬을거임
         seg.header().rst = true; // 오류 flag 설정
         auto ackno = _receiver.ackno(); // 사실 이거 ackno win이 rst에서는 필요없을거같긴한데 일단 보내줌
+        auto win = _receiver.window_size();
         seg.header().win = win;
         if (ackno.has_value()){
             seg.header().ackno = ackno.value(); // syn 일때는 ackno없음 그냥 isn값만 seqno에 실어서 보냄, 이제 이걸 받은쪽이 isn설정됬으니가 ackno응답이 가능해서 실어서줌
@@ -81,13 +125,14 @@ void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_
 }
 
 void TCPConnection::end_input_stream() {
-    _sender.stream.end_input(); // 종료를 위해 이걸 호출
+    _sender.stream_in().end_input(); // 종료를 위해 이걸 호출
     _sender.fill_window(); // fin을 채워서 segment만들어둠
     send_segment(); // 세그먼트 전송
 }
 
 void TCPConnection::connect() { // 연결하는 함수 3hand shake 의 syn보내는 첫부분만 담당
-    if (_sender.next_seqno() > 0) { // 방어로직 - 이미 syn을 보냈으면 1이상이라서 더이상 connect호출해도 무시해야함
+    _active = true; // 켜주고
+    if (_sender.next_seqno_absolute() > 0) { // 방어로직 - 이미 syn을 보냈으면 1이상이라서 더이상 connect호출해도 무시해야함
         return;
     }
     _sender.fill_window();
@@ -96,19 +141,19 @@ void TCPConnection::connect() { // 연결하는 함수 3hand shake 의 syn보내
 
 void TCPConnection::send_segment() { // queue에 쌓인걸 보내는함수
     auto& queue = _sender.segments_out(); // auto로 하면 타입 신경안써도 됨 그리고 & 붙여서 참조로 가져와야 pop이 먹힘 복사안해서
-    while(!queue.empty()){. //while(queue){ 아 파이썬아니라서 이거 안됨 // queue가 존재하면
+    while(!queue.empty()){ //while(queue){ 아 파이썬아니라서 이거 안됨 // queue가 존재하면
         TCPSegment seg;
-        size_t win;
+        // size_t win;
         seg = queue.front();
         // 지금은 seqno만 있고 ackno랑 win을 알아야하는데 이건 Sender말고 Reciever에서 알고있음
 
-        win = _receiver.window_size();
+        auto win = _receiver.window_size();
         auto ackno = _receiver.ackno(); // auto는 뒤에 바로 결과 값이 있어야 사용가능
         seg.header().win = win;
-        if (ackno.has_value()){
+        if (ackno.has_value()){ 
             seg.header().ackno = ackno.value(); // syn 일때는 ackno없음 그냥 isn값만 seqno에 실어서 보냄, 이제 이걸 받은쪽이 isn설정됬으니가 ackno응답이 가능해서 실어서줌
             // optional 타입이라 값을 받으려면 .value()로 까야함
-            seg.header().ack = true;
+            seg.header().ack = true; // ackno가 있으면 무조건 대답이니까 
         }
         
         _segments_out.push(seg);
@@ -130,6 +175,7 @@ TCPConnection::~TCPConnection() { // 소멸자
             seg = queue.back(); // 젤뒤에 추가됬을거임
             seg.header().rst = true; // 오류 flag 설정
             auto ackno = _receiver.ackno(); // auto는 뒤에 바로 결과 값이 있어야 사용가능
+            auto win = _receiver.window_size();
             seg.header().win = win;
             if (ackno.has_value()){
                 seg.header().ackno = ackno.value(); // syn 일때는 ackno없음 그냥 isn값만 seqno에 실어서 보냄, 이제 이걸 받은쪽이 isn설정됬으니가 ackno응답이 가능해서 실어서줌
--- libsponge/tcp_connection.hh
+++ libsponge/tcp_connection.hh
@@ -29,7 +29,7 @@ class TCPConnection {
     //! \name "Input" interface for the writer
     //!@{
 
-    void TCPConnection::send_segment(); // segment 보내는함수
+    void send_segment(); // segment 보내는함수
 
     //! \brief Initiate a connection by sending a SYN segment
     void connect();
@@ -83,7 +83,7 @@ class TCPConnection {
     //! \brief Is the connection still alive in any way?
     //! \returns `true` if either stream is still running or if the TCPConnection is lingering
     //! after both streams have finished (e.g. to ACK retransmissions from the peer)
-    bool active() const { return _active; } // 이걸 반환해야함
+    bool active() const;  // 이걸 반환해야함
     //!@}
 
     //! Construct a new connection from a configuration
